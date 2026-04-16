5a8c55c5c952af6124ddcf0fa448a935d7c3f63b
Abhay3011994 <abhay3011994@gmail.com>
2026-04-16 15:38:39 +0530

    ok

--- playwright.config.js
+++ playwright.config.js
@@ -16,7 +16,7 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-   reporter: [['html', { open: 'never', host: '0.0.0.0' }]],
+  reporter: [['html', { open: 'never', host: '0.0.0.0' }]],
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+ // reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
