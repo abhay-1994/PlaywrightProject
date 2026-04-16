4df3b1a2312c67db7b9125955901afe07f42aba0
Abhay3011994 <abhay3011994@gmail.com>
2026-04-16 13:23:25 +0530

    Updated

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,3 +3,6 @@ import {test} from '@playwright/test';
 test("First test", async({page})=>{
 await page.goto("https://www.google.com/");
 })
+test("Second test", async({page})=>{
+await page.goto("https://www.github.com/");
+})
